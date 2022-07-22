45905c4eac8bd00d812d9f686fc3daee1896a6b4
zhubin <zhubinnju@outlook.com>
2022-07-22 14:24:35 +0800

    cpr test

--- src/spider.cc
+++ src/spider.cc
@@ -1,5 +1,20 @@
 #include <cstdio>
-int main()
+#include <cstring>
+#include <iostream>
+#include <string>
+
+#include "cpr/cpr.h"
+int main(int argc, char **argv)
 {
-    printf("hello word");
+    cpr::Response r = cpr::Get(cpr::Url{"https://www.bilibili.com/?spm_id_from=333.337.0.0"});
+
+    // std::cout << "Status code: " << r.status_code << '\n';
+    // std::cout << "Header:\n";
+    // for (const std::pair<const std::basic_string<char>, std::basic_string<char>> &kv : r.header)
+    // {
+    //     std::cout << '\t' << kv.first << ':' << kv.second << '\n';
+    // }
+    // std::cout << "Text: " << r.text << '\n';
+    printf("%s", r.text.c_str());
+    return 0;
 }
---
+++ src/spider.h
@@ -0,0 +1,4 @@
+#ifndef SRC_SPIDER_H
+#define SRC_SPIDER_H
+
+#endif
