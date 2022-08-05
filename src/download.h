ae31e2dbb10daef75ae831c41b13e96f1672debb
zhubin <zhubinnju@outlook.com>
2022-08-05 17:54:46 +0800

    download task

--- src/download.h
+++ src/download.h
@@ -1,17 +1,42 @@
 #ifndef SRC_DOWNLOAD_H
 #define SRC_DOWNLOAD_H
+#include <cstdio>
 #include <cstring>
+#include <memory>
 #include <string>
 
+#include "cpr/cpr.h"
 namespace spider {
 
 // 数据下载模块
 class DownloadTask {
 public:
+    static DownloadTask* from_videoinfo();
     virtual void start_download() = 0;
-    virtual void stop_download() = 0;
+    virtual std::string get_title();
+    virtual ~DownloadTask();
+
+protected:
+    // 文件路径
+    std::unique_ptr<std::string> path_;
+    std::unique_ptr<cpr::Response> response_;
     DownloadTask();
-    ~DownloadTask();
+
+public:
+    std::string get_path() {
+        return *path_;
+    };
+};
+
+class UgcVideoDownloadTask : public DownloadTask {
+protected:
+    int totol_video_size;
+    std::unique_ptr<std::FILE> file_;
+
+public:
+    void parse_url();
+    std::string get_title() override;
+    void start_download() override;
 };
 
 }  // namespace spider
