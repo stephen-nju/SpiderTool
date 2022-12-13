5b16b715b05c93498961a25a5eb04d84994b7390
zhubin <zhubinnju@outlook.com>
2022-12-13 13:52:42 +0800

    install project

--- src/main.cc
+++ src/main.cc
@@ -7,6 +7,7 @@
 #include "cpr/cpr.h"
 #include "spdlog/spdlog.h"
 #include "spider_task.h"
+
 int main(int argc, char** argv) {
     spdlog::info("start spider......\n");
     absl::string_view url = "https://www.bilibili.com/video/BV1U741177AV?from=search&seid=3177691572837221964";
