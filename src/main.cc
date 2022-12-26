2857a595d96abcda0e4fc00c65014b188ae45c77
zhubin <zhubinnju@outlook.com>
2022-12-26 17:53:18 +0800

    absl flag

--- src/main.cc
+++ src/main.cc
@@ -4,13 +4,16 @@
 #include <memory>
 #include <string>
 
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
 #include "cpr/cpr.h"
 #include "spdlog/spdlog.h"
 #include "spider_task.h"
+ABSL_FLAG(std::string, url, "", "video url");
 
 int main(int argc, char** argv) {
-    spdlog::info("start spider......\n");
-    absl::string_view url = "https://www.bilibili.com/video/BV1U741177AV?from=search&seid=3177691572837221964";
+    absl::ParseCommandLine(argc, argv);
+    std::string url = absl::GetFlag(FLAGS_url);
     const absl::string_view dir = absl::string_view("./");
 
     spider::SpiderTask* task = new spider::SpiderTask(url, dir);
